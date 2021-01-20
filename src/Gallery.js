cbf6eb1cfce471c1bfe2c1a8fdfed46af8a05ed2
hajri houssem <houssemeddine.hajri@sesame.com.tn>
2021-01-20 13:03:23 +0100

    Create a React app and publish it to GitHub Pages

--- src/Gallery.js
+++ src/Gallery.js
@@ -5,9 +5,9 @@ return (
 <div class="row">
   <div class="column">
     <img src="https://www.w3schools.com/images/picture.jpg" alt="image"></img>
-    <img src="https://www.w3schools.com/images/picture.jpg" alt="image"></img>
-    <img src="https://www.w3schools.com/images/picture.jpg" alt="image"></img>
-    <img src="https://www.w3schools.com/images/picture.jpg" alt="image"></img>
+    <img src="https://image.shutterstock.com/image-photo/plant-based-meal-prep-600w-1115402558.jpg" alt="image"></img>
+    <img src="https://image.shutterstock.com/image-photo/korean-buffet-food-600w-1352752961.jpg" alt="image"></img>
+
   </div>
 
 </div>
